65c7c0ea8728faf2942c8b7443c16d0b558dc4f4
Jhon-Zuluaga <jhonxyleh@gmail.com>
2026-04-21 11:05:11 -0500

    implement file upload with multer in NestJS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {FileInterceptor} from '@nestjs/platform-express';
+import { Multer } from 'multer'; // Necesario para que typscpt recoznoca express
 
-@Controller()
+/**
+ * Controlador para subida de archivos
+ * Ruta base: /files
+ */
+
+@Controller('files')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  
+  // Post /files/upload - Recibe un archivo del form-data en el campo file(postman)
+  @Post('upload')
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // Intercepta y procesa el archivo con Multer
+  @UseInterceptors(FileInterceptor('file')) 
+  // Extrae el archivo del request
+  uploadFile(@UploadedFile() file: Express.Multer.File){
+    // file contiene: originalmente, mimetype, size, buffer etc..
+    console.log(file);
+    return {
+      Mesasge: 'File uploaded successfully',
+    };
   }
 }
+
+// Install
+
+// npm run start:dev
+// npm i --save @nestjs/serve-static
+// npm i @nestjs/platform-express
+
+// POST: http://localhost:3000/files/upload?file
+// form-data, file value: (upload image) send
+
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MulterModule } from '@nestjs/platform-express';
+import {ServeStaticModule} from '@nestjs/serve-static'
+import { join } from 'path';
 
-@Module({
-  imports: [],
+/**
+ * Módulo principal de la aplicación
+ */
+
+@Module({ 
+  imports: [
+    // Configuracion multer globalmente: Los archivos se guardan en la carpeta /uploads
+    MulterModule.register({
+      dest: './uploads',
+    }),
+
+    // Sirve los archivos de ./uploads como estaticos
+    // Accesibles desde: http://localhost:3000/nombre-archivo.jpg
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
